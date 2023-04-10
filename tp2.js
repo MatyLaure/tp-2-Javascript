7e0d25db05fff866ae19e31b8e335532e3d43f37
Matias Andres Garcia Laure <garcialaurema@gmail.com>
2023-04-09 21:06:24 -0300

    tp2 Javascript

---
+++ tp2.js
@@ -0,0 +1,11 @@
+const list = [
+    "Matias",
+    33,
+    true,
+    new Date(1990,2,14) {
+        titulo: "Cementerio de animales",
+        autor: "Stephen King",
+        fecha: new Date (1970,01,16),
+        url: "https://www.casadellibro.com/libro-cementerio-de-animales/9788497930994/940957"
+    },
+];
